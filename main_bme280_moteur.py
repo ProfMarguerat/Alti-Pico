6f7c2e811b0fc2450982cd853f695cfd96be7edb
ProfMarguerat <marguerat.thomas@lppstjosephgabarret.com>
2026-05-06 15:43:46 +0200

    Update main_bme280_moteur.py

--- main_bme280_moteur.py
+++ main_bme280_moteur.py
@@ -225,7 +225,7 @@ elapsed_time = 0
 afficheur = 0
 apogee = 0
 atterrissage = 0 
-alt2=altitude0+2
+alt2=altitude0+4
 altitude_max = -999
 porte, elapsed_time_p, start_p  = 1,0,0 #Porte = 1 : porte férmée
 utime.sleep (0.1)
@@ -273,7 +273,7 @@ while True:
         if boutton.value()==0:
             altitude_max=-999
             altitude0 = altitude_IBF(pressure_hPa)
-            alt2=altitude0+2
+            alt2=altitude0+4
             elapsed_time = 0
             apogee = 0
             start = 0
@@ -329,7 +329,7 @@ while True:
     
     #alt=altitude0+2
     #print (alt2)
-    # Démarrage chrono lorsque l'altitude dépasse celle de départ plus 2m pour pallier aux dérives de pression :
+    # Démarrage chrono lorsque l'altitude dépasse celle de départ plus 4m pour pallier aux dérives de pression :
     if altitude > alt2 and start == 0:
         #print ("c'est parti !")
         tableau_valeur = open(fichier,'a')
